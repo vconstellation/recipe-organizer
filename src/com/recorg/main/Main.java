7aaff0aca7339e32c3ae7306df7f22796e47bcb7
Paweł Merek <pawel.merek92@gmail.com>
2020-10-09 20:51:12 +0200

    Add Main.class to test enities creation

---
+++ src/com/recorg/main/Main.java
@@ -0,0 +1,42 @@
+package com.recorg.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.recorg.entity.Nutrition;
+import com.recorg.entity.Product;
+import com.recorg.entity.Recipe;
+import com.recorg.entity.RecipeRating;
+import com.recorg.entity.RecipeStep;
+import com.recorg.entity.User;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Nutrition.class)
+				.addAnnotatedClass(Product.class)
+				.addAnnotatedClass(Recipe.class)
+				.addAnnotatedClass(RecipeRating.class)
+				.addAnnotatedClass(RecipeStep.class)
+				.addAnnotatedClass(User.class)
+				.buildSessionFactory();
+
+			Session session = factory.getCurrentSession();
+			try {
+					session.beginTransaction();
+					session.getTransaction().commit();
+
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			finally {
+				session.close();
+				factory.close();
+			}
+	}
+
+}
